7cb58b50b53113bc91a6da169ad54ff7ef59df3c
Nikita Gridinskiy <nikita.gridinskiy@gmail.com>
2019-06-22 22:47:01 -0700

    added string reversal example

---
+++ src/reversey.elm
@@ -0,0 +1,33 @@
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+-- MAIN
+main = Browser.sandbox { init = init, update = update, view = view }
+
+-- MODEL
+type alias Model = { content: String }
+
+init: Model
+init = { content = ""}
+
+-- UPDATE
+
+type Msg = Change String
+update: Msg -> Model -> Model
+update message model =
+    case message of
+         Change newContent -> 
+            {model | content = newContent}
+
+-- VIEW
+
+view: Model -> Html Msg
+view model =
+    div [] [
+        input [placeholder "Text to reverse", value model.content, onInput Change] [],
+        div [] [
+            text (String.reverse model.content)
+        ]
+    ]
