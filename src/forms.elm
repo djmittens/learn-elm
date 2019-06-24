e052f2889cdc2ae0a2439ebf918920d83a116c1b
Nikita Gridinskiy <nikita.gridinskiy@gmail.com>
2019-06-23 21:29:09 -0700

    improving forms example.

--- src/forms.elm
+++ src/forms.elm
@@ -1,7 +1,7 @@
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onInput, onClick)
 
 
 -- MAIN
@@ -16,17 +16,18 @@ type alias Model =
     {
         name: String,
         password: String,
-        passwordAgain: String
+        passwordAgain: String,
+        validated: Bool
     }
 
 init: Model
 init = 
-    Model "" "" ""
+    Model "" "" "" False
 
 -- UPDATE
 
 type Msg =
-    Name String | Password String | PasswordAgain String
+    Name String | Password String | PasswordAgain String | Validate
 
 update : Msg -> Model -> Model
 update msg model = 
@@ -39,6 +40,9 @@ update msg model =
 
         PasswordAgain password ->
             { model | passwordAgain = password }
+        
+        Validate ->
+            { model | validated = not model.validated }
 
 -- VIEW
 view : Model -> Html Msg
@@ -47,7 +51,9 @@ view model =
         viewInput "text" "Name" model.name Name,
         viewInput "password" "Password" model.password Password,
         viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain,
-        viewValidation model
+        button [onClick Validate ] [text "validate"] ,
+        if model.validated then viewValidation model
+        else div [][]
     ]
 
 viewInput : String -> String -> String -> (String -> msg) -> Html msg
@@ -56,7 +62,12 @@ viewInput t p v toMsg =
 
 viewValidation : Model -> Html msg
 viewValidation model =
-    if model.password == model.passwordAgain then
-        div [ style "color" "green" ] [ text "OK" ]
+    if String.length model.password < 8 then   
+        error "Password has to be greater than 8 characters!"
+    else if model.password == model.passwordAgain then
+        ok
     else
-        div [ style "color" "red" ] [ text " Password do not match !" ]
+        error " Passwords do not match !"
+
+ok = div [ style "color" "green" ] [ text "OK" ]
+error err = div [ style "color" "red" ] [ text err ]
